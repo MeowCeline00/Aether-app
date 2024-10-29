8c4b46ce5cc8211c5f7e7b19da1818bdd4addf21
Celine_00 <132836456+MeowCeline00@users.noreply.github.com>
2024-10-29 06:28:17 -0700

    adjust layout

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { View, Image, TextInput, Text, Pressable } from "react-native";
 import { styled } from "nativewind";
 import { useRouter } from "expo-router";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 
 const StyledView = styled(View);
 const StyledImage = styled(Image);
@@ -14,24 +14,24 @@ export default function HomePage() {
   const [username, setUserName] = useState("");
   const [password, setPassWord] = useState("");
   const router = useRouter();
-  
+
   const handleLogIn = () => {
-    router.push({pathname: "/homePage", params: {username}});
+    router.push({ pathname: "/homePage", params: { username } });
   };
 
   return (
     <LinearGradient
-      colors={['#c4d3ff', '#ffffff']} 
-      start={{ x: 0, y: 0 }} 
-      end={{ x: 0, y: 1 }} 
+      colors={["#88B5DF", "#ffffff"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 0.8 }}
       style={{ flex: 1 }}
     >
       <StyledView className="flex-1 justify-center items-center p-4">
-        <StyledView className="items-center mb-12 mt-12"> 
+        <StyledView className="items-center mb-24 mt-12">
           <StyledImage
             source={require("@/images/aether_logo.png")}
-            resizeMode="contain" 
-            className="w-64 h-32"  
+            resizeMode="contain"
+            className="w-64 h-32"
           />
         </StyledView>
         <StyledView className="w-full px-8">
@@ -48,15 +48,23 @@ export default function HomePage() {
             onChangeText={setPassWord}
             className="mb-4 h-12 bg-white rounded-full px-4"
           />
-          <StyledPressable className="bg-blue-500 h-12 rounded-full justify-center items-center mb-4" onPress={handleLogIn}>
+          <StyledPressable
+            className="bg-blue-500 h-12 rounded-full justify-center items-center mb-4"
+            onPress={handleLogIn}
+          >
             <StyledText className="text-white text-lg">Sign In</StyledText>
           </StyledPressable>
           <StyledPressable onPress={() => console.log("Continue as guest")}>
-            <StyledText className="text-center text-lg">Continue as guest</StyledText>
+            <StyledText className="text-center text-lg">
+              Continue as guest
+            </StyledText>
           </StyledPressable>
           <StyledPressable onPress={() => router.push("/register")}>
             <StyledText className="text-center text-sm mt-2">
-              Don't have an account? <StyledText className="font-bold underline">Create one.</StyledText>
+              Don't have an account?{" "}
+              <StyledText className="font-bold underline">
+                Create one.
+              </StyledText>
             </StyledText>
           </StyledPressable>
         </StyledView>
